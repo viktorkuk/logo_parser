b0bf3db3458d0aabeab4f155890fd6b9f2d94adf
Viktor Kukarin <v.kukarin@b2blogger.com>
2021-03-08 15:10:02 +0200

    add letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(publizerr.com)#(publizerr.com www.publizerr.com)
+domains=(publizerr.com) # (publizerr.com www.publizerr.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
--- run-me.sh
+++ run-me.sh
@@ -5,3 +5,6 @@ docker-compose run --rm artisan migrate
 docker-compose run --rm npm install
 docker-compose run --rm npm run dev
 
+chmod +x init-letsencrypt.sh
+sudo ./init-letsencrypt.sh
+
