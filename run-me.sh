6e65a09f7ede2ba369defc85444f81da24187aab
Viktor Kukarin <v.kukarin@b2blogger.com>
2021-03-10 14:33:45 +0200

    ui spa

--- run-me.sh
+++ run-me.sh
@@ -5,6 +5,6 @@ docker-compose run --rm composer install
 docker-compose run --rm npm install
 docker-compose run --rm npm run dev
 
-chmod +x init-letsencrypt.sh
-sudo ./init-letsencrypt.sh
+#chmod +x init-letsencrypt.sh
+#sudo ./init-letsencrypt.sh
 
